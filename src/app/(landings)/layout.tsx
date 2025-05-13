20d94ec02688c81293ff4163a8eabcd3018e835f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-05-13 23:37:47 +0600

    added next Link and nav and footer

--- src/app/(landings)/layout.tsx
+++ src/app/(landings)/layout.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 interface IProps {
   children: React.ReactNode;
 }
 
 export default function AppLayout(props: IProps) {
-  return <main>{props.children}</main>;
+  return (
+    <>
+      <nav>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about-us">About us</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>{props.children}</main>
+
+      <footer>Footer</footer>
+    </>
+  );
 }
--- src/app/(landings)/page.tsx
+++ src/app/(landings)/page.tsx
@@ -1,17 +1,3 @@
-import FormInput from "@/components/form/FormInput";
-import { Button } from "@/components/ui/Button";
-import Heading from "@/components/ui/Heading";
-
 export default function Home() {
-  return (
-    <div>
-      <Heading variant="h2">Sub Title</Heading>
-      <Button variant="secondary">Submit</Button>
-
-      <form>
-        <FormInput label="Email" type="email" required />
-        <FormInput label="Password" type="password" required />
-      </form>
-    </div>
-  );
+  return <div>Homepage</div>;
 }
